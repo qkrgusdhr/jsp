6bb1376d2696f07a4f3d150f17da526056af4dce
21-manic-017g <21-manic-017g@21-manic-017>
2024-06-03 17:45:17 +0900

    문제

--- q0522/src/q1309/Main.java
+++ q0522/src/q1309/Main.java
@@ -7,23 +7,28 @@ public class Main {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		long fact[] = new long[n+1];
-		fact[0] = 1;
-		fact[1] = 1;
-		
-		for(int i = 1; i <= n; i++) {
-			fact[i] = i * fact[i-1];
-		}
-		
-		for(int i = n; i >= 1; i--) {
-			System.out.print(i + "!" + " = ");
-			if(i == 1) {
-				System.out.println(i + " * " + 1);
-			}else {
-				System.out.println(i + " * " + (i-1) + "!");
+
+		if (n == 0) {
+			System.out.println("0! = 1");
+		} else {
+			long fact[] = new long[n + 1];
+			fact[0] = 1;
+			fact[1] = 1;
+			for (int i = 1; i <= n; i++) {
+				fact[i] = i * fact[i - 1];
 			}
+
+			for (int i = n; i >= 1; i--) {
+				System.out.print(i + "!" + " = ");
+				if (i == 1) {
+					System.out.println(i);
+				} else {
+					System.out.println(i + " * " + (i - 1) + "!");
+				}
+			}
+			System.out.println(fact[n]);
+			sc.close();
 		}
-		System.out.println(fact[n]);
-		sc.close();
+
 	}
 }
