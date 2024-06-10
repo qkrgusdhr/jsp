5bc30755ae34a65e330ec88e4cb70f0778a274f4
21-manic-017g <21-manic-017g@21-manic-017>
2024-06-10 13:36:39 +0900

    0610 mvc2 게시판 구현

--- 0528/src/main/java/utils/JSFunction.java
+++ 0528/src/main/java/utils/JSFunction.java
@@ -1,30 +1,38 @@
 package utils;
 
 
+import java.io.PrintWriter;
+
+
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.jsp.JspWriter;
 
 public class JSFunction {
-	public static void alertLocation(String msg, String url, JspWriter out) {
+	public static void alertLocation(HttpServletResponse response, String url, String string) {
 		try {
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = response.getWriter();
 			String script = ""
 						  + "<script>"
-						  + "	alert('" + msg +"');"
+						  + "	alert('" + response +"');"
 						  + "	location.href = '" + url +"';"
 						  + "</script>";
-			out.println(script);
+			writer.print(script);
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
 	
-	public static void alertBack(String msg, JspWriter out) {
+	public static void alertBack(HttpServletResponse response, String msg, JspWriter out) {
 		try {
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = response.getWriter();
 			String script = ""
 						  + "<script>"
 						  + " 	alert('" + msg + "');"
 						  + " 	history.back();"
 						  +	"</script>";
-			out.println(script);
+			writer.print(script);
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
